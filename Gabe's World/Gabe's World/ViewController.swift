82cdee204c004b83788daa71268274d7d842f259
XxDizzyxX <49726117+XxDizzyxX@users.noreply.github.com>
2019-04-17 11:59:45 -0500

    Add files via upload

--- Gabe's World/Gabe's World/ViewController.swift
+++ Gabe's World/Gabe's World/ViewController.swift
@@ -9,15 +9,17 @@
 import UIKit
 //4 -
 import AVFoundation
-
 class ViewController: UIViewController {
 
     //5 -
     var songPlayer = AVAudioPlayer()
     
+    //15 -
+    var hasBeenPaused = false
+    
     //6 -
     func prepareSongAndSession() {
-        
+    
         do {
             //7 - Insert the song form our Bundle into our AVAudioPlayer
             songPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Marshmello%2C%20Bastille%20-%20Happier%20(Lyrics)", ofType:"mp3")!))
@@ -26,28 +28,51 @@ class ViewController: UIViewController {
             
             //9 - Create an audio session
             let audioSession = AVAudioSession.sharedInstance()
-            do {}
+            do {
+                //10 - Set our session category to playback music
+                try audioSession.setCategory(AVAudioSessionCategoryPlayback)
+                //11 -
         } catch let sessionError {
+         
             print(sessionError)
+        }
+        //12 -
+    } catch let songPlayerError {
+        print(songPlayerError)
         }
     }
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //13
+        prepareSongAndSession()
     }
 
     @IBAction func play(_ sender: Any) {
+        //14
+        songPlayer.play()
     }
+    
     @IBAction func pause(_ sender: Any) {
+        //16
+        if songPlayer.isPlaying {
+            songPlayer.pause()
+            hasBeenPaused = true
+        } else {
+            hasBeenPaused = false
+        }
+        
     }
     @IBAction func restart(_ sender: Any) {
+        //17
+        if songPlayer.isPlaying || hasBeenPaused {
+            songPlayer.stop()
+            songPlayer.currentTime = 0
+            
+            songPlayer.play()
+        } else {
+            songPlayer.play()
+        }
     }
     
 }
-
